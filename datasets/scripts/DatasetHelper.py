a6fe70b789e859d32241f5f298ff653d2fced7e9
Keoma Brun <keoma.brun@inria.fr>
2017-12-21 14:45:05 +0100

    update dataset_info script

--- datasets/scripts/DatasetHelper.py
+++ datasets/scripts/DatasetHelper.py
@@ -1,60 +1,69 @@
 from scipy.spatial import distance
-import urllib
 import json
-import datetime
 import pandas as pd
-
+import numpy as np
+import os
 
 def load_dataset(raw_file_path):
-    df_list = {}
-    df = pd.read_csv(raw_file_path)
+    if os.path.isfile(raw_file_path + ".csv"):
+        raw_file_path += ".csv"
+    elif os.path.isfile(raw_file_path + ".csv.gz"):
+        raw_file_path += ".csv.gz"
+    else:
+        print "Files supported: .csv and .csv.gz"
+        quit()
 
-    group_trans = df.groupby("transctr")
-    for transctr, df_trans in group_trans:
+    df = pd.read_csv(raw_file_path,
+                     dtype={"timestamp": np.str,
+                           "mac": np.str,
+                           "frequency": np.uint8,
+                           "length":np.uint32,
+                           "rssi": np.int32,
+                           "crc": np.bool,
+                           "expected": np.bool,
+                           "srcmac": np.str,
+                           "transctr": np.uint32,
+                           "pkctr": np.uint32,
+                           "nbpackets": np.uint32,
+                           "txpower": np.uint16,
+                           "txifdur": np.uint32,
+                           "txpksize": np.uint32,
+                           "txfillbyte": np.str
+                           },
+                     date_parser = lambda timestamp: pd.datetime.strptime(timestamp, "%Y-%m-%d_%H.%M.%S"),
+                     parse_dates = {'datetime': ['timestamp']},
+                     #index_col = [0],  # make datetime column as index
+                     )
 
-        # remove wrong values
-        df_trans = df_trans.drop_duplicates()
-        df_trans = df_trans[(df_trans.crc == 1) & (df_trans.expected == 1)]
+    # clean dataset
+    df = df.drop_duplicates()
+    df = df[(df.crc == 1) & (df.expected == 1)]
+    df.drop('crc', axis=1, inplace=True)
+    df.drop('expected', axis=1, inplace=True)
 
-        if transctr in df_list:
-            df_list[transctr].append(df_trans)
-        else:
-            df_list[transctr] = df_trans
+    return df
 
-    return df_list
-
-
-def helper(df, testbed):
-    helper = {
-        "node_count": -1,
-        "tx_count": None,
-        "channel_count": None,
-        "testbed": testbed,
-        "data":  None,
-    }
-
-    # extract dataset properties
-
-    helper["node_count"] = len(df.groupby("mac"))
-    helper["channel_count"] = len(df.groupby("frequency"))
+def helper(df):
     if "nbpackets" in df.keys():
-        helper["tx_count"] = df["nbpackets"].iloc[0]
+        tx_count = df["nbpackets"].iloc[0]
     else:
-        helper["tx_count"] = df["txnumpk"].iloc[0]
-    helper["tx_ifdur"] = df["txifdur"].iloc[0]
+        tx_count = df["txnumpk"].iloc[0]
+
     if "txpksize" in df.keys():
-        helper["tx_length"] = df["txpksize"].iloc[0]
+        tx_length = df["txpksize"].iloc[0]
     else:
-        helper["tx_length"] = df["txlength"].iloc[0]
-    helper["transaction_count"] = len(df.groupby(["transctr", "srcmac"]))/helper["node_count"]
-    start_date = datetime.datetime.strptime(df["timestamp"].iloc[0], "%Y-%m-%d_%H.%M.%S")
-    end_date = datetime.datetime.strptime(df["timestamp"].iloc[-1], "%Y-%m-%d_%H.%M.%S")
-    helper["start_date"] = start_date.strftime("%Y-%m-%d %H:%M:%S")
-    helper["end_date"] = end_date.strftime("%Y-%m-%d %H:%M:%S")
-    helper["duration"] = round((end_date - start_date).seconds/3600.0, 2)
+        tx_length = df["txlength"].iloc[0]
 
-    return helper
-
+    return {
+        "node_count": len(df.groupby("mac")),
+        "channel_count": len(df.frequency.unique()),
+        "tx_count": int(tx_count),
+        "tx_ifdur": int(df["txifdur"].iloc[0]),
+        "tx_length": int(tx_length),
+        "transaction_count": len(df.groupby("transctr")),
+        "start_date": df.datetime.iloc[0].strftime("%Y-%m-%d %H:%M:%S"),
+        "end_date": df.datetime.iloc[-1].strftime("%Y-%m-%d %H:%M:%S"),
+    }
 
 def get_nodes_info(testbed):
     target_url = "../../metas/{0}.json".format(testbed)
@@ -68,6 +77,13 @@ def get_coords(node_list, mac):
         if "mac" in node and node["mac"] == mac:
             return float(node["x"]), float(node["y"]), float(node["z"])
     return 0, 0, 0
+
+def get_max_edge_count(node_count):
+    max_link_count = 0
+    node_count = node_count -1
+    for i in range(node_count):
+        max_link_count += node_count - i
+    return max_link_count
 
 
 def get_dist(node_list, mac1, mac2):
--- datasets/scripts/dataset_info.py
+++ datasets/scripts/dataset_info.py
@@ -7,23 +7,25 @@
 #   number of channels
 #   duration
 #   size
+#   ...
 #
 # The generated file are located here:
-#    processed/<site>/<data>/info.json
+#    processed/<site>/<date>_info.json
 #
 # the format is json:
 # {
 #   'nb_nodes': <int>,
 #   'nb_channels': <int>,
-#   'duration': <int> (in hours)
+#   'duration': <int>, (in hours)
+#   ...
 # }
 
 # ============================== imports ======================================
 
 import os
 import argparse
-import pandas as pd
 import json
+import networkx as nx
 
 import DatasetHelper
 
@@ -38,108 +40,63 @@ OUT_PATH = "../processed"
 def main():
 
     # parsing user arguments
-
     parser = argparse.ArgumentParser()
     parser.add_argument("testbed", help="The name of the testbed data to process", type=str)
     parser.add_argument("date", help="The date of the dataset", type=str)
     args = parser.parse_args()
 
     # load the dataset
-
     raw_file_path = "{0}/{1}/{2}".format(RAW_PATH, args.testbed, args.date)
-    if os.path.isfile(raw_file_path + ".csv"):
-        raw_file_path += ".csv"
-    elif os.path.isfile(raw_file_path + ".csv.gz"):
-        raw_file_path += ".csv.gz"
-    else:
-        print "Files supported: .csv and .csv.gz"
-        quit()
+    df = DatasetHelper.load_dataset(raw_file_path)
 
-    df = pd.read_csv(raw_file_path)
+    dtsh = DatasetHelper.helper(df)
 
-    # clean dataset
-    df = df.drop_duplicates()
-    df = df[(df.crc == 1) & (df.expected == 1)]
+    # compute degree and radius
+    net_degree_list = []
+    net_radius_list = []
+    for name, df_goup in df.groupby(["transctr"]):
+        if df_goup.empty:
+            continue
 
-    dtsh = DatasetHelper.helper(df, args.testbed)
+        # create graph
+        G = nx.Graph()
+        G.add_nodes_from(df_goup.srcmac)
+        G.add_edges_from(df_goup.groupby(["srcmac", "mac"]).groups.keys())
 
-    # get nodes info
+        # calculate network degree
+        max_edge_count = DatasetHelper.get_max_edge_count(G.number_of_nodes())
+        net_degree = G.number_of_edges() / float(max_edge_count)
 
-    node_list = DatasetHelper.get_nodes_info(args.testbed)
+        # save degree and radius
+        net_degree_list.append(net_degree)
+        net_radius_list.append(nx.radius(G))
 
-    # compute paths list
-    path_list = []
-    df["hash"] = df.apply(lambda row: (get_hash(row["mac"], row["srcmac"])), axis=1)
-    for hash, link in df.groupby(["hash"]):
-        AtoB = {}
-        BtoA = {}
-
-        # compute PDR for each link and separate A->B and B->A links
-        for m, gr in link.groupby(["mac"]):
-            rx_count = len(gr)
-            rx_expected = dtsh["transaction_count"] * dtsh["tx_count"] * dtsh["channel_count"]
-            avg_pdr = rx_count * 100 / rx_expected
-
-            # compute PDR for each frequency
-            pdr_list = []
-            for channel, df_freq in gr.groupby(["frequency"]):
-                rx_count = len(df_freq)
-                rx_expected = dtsh["transaction_count"] * dtsh["tx_count"]
-                pdr = rx_count * 100 / rx_expected
-                pdr_list.append((channel, pdr))
-
-            res = { "src": gr["srcmac"].iloc[0],
-                    "dst": m,
-                    "distance": DatasetHelper.get_dist(node_list, gr["srcmac"].iloc[0], m),
-                    "PDR": {
-                        "average": avg_pdr,
-                        "channel": {channel: pdr for (channel, pdr) in pdr_list}
-                    }
-            }
-            if check_hash(m, gr["srcmac"].iloc[0], hash):
-                AtoB = res
-            else:
-                BtoA = res
-            print res
-
-        # add path
-        path_list.append([AtoB, BtoA])
+    # calculate average degree and radius
+    avg_net_degree = sum(net_degree_list) / float(len(net_degree_list))
+    avg_net_radius = sum(net_radius_list) / float(len(net_radius_list))
 
     # format collected information
-
     json_data = {
-        "global": {
-            "start_date": dtsh["start_date"],
-            "end_date": dtsh["end_date"],
-            "duration": dtsh["duration"],
-            "nb_nodes": dtsh["node_count"],
-            "nb_channels": dtsh["channel_count"],
-            "transaction_count": dtsh["transaction_count"],
-            "tx_count": dtsh["tx_count"],
-            "tx_ifdur": dtsh["tx_ifdur"],
-            "tx_length": dtsh["tx_length"],
-            },
-        "paths": path_list
+        "start_date": dtsh["start_date"],
+        "end_date": dtsh["end_date"],
+        "nb_nodes": dtsh["node_count"],
+        "nb_channels": dtsh["channel_count"],
+        "transaction_count": dtsh["transaction_count"],
+        "tx_count": dtsh["tx_count"],
+        "tx_ifdur": dtsh["tx_ifdur"],
+        "tx_length": dtsh["tx_length"],
+        "degree": avg_net_degree,
+        "radius": avg_net_radius,
     }
+    print(json.dumps(json_data, indent=4))
 
     # write the information to a file
-
-    path = "{0}/{1}/{2}/".format(OUT_PATH, args.testbed, args.date)
+    path = "{0}/{1}/".format(OUT_PATH, args.testbed)
     if not os.path.exists(path):
         os.makedirs(path)
-    with open(path + "info.json", 'w') as output_file:
-        json.dump(json_data, output_file)
+    with open(path + "{0}_info.json".format(args.date), 'w') as output_file:
+        json.dump(json_data, output_file, indent=4)
 
-
-def get_hash(A, B):
-    return ''.join(sorted([A, B]))
-
-
-def check_hash(A, B, hash):
-    if ''.join([A, B]) == hash:
-        return True
-    else:
-        return False
 
 if __name__ == '__main__':
     main()
